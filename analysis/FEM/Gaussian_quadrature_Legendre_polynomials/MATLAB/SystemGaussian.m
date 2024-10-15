c8a9c645b203c6461aec6e5ba524aa0a630a04a5
NQBH <nguyenquanbahong@gmail.com>
2024-10-15 14:31:14 +0700

    Gaussian quadrature & Legendre polynomials

---
+++ analysis/FEM/Gaussian_quadrature_Legendre_polynomials/MATLAB/SystemGaussian.m
@@ -0,0 +1,36 @@
+clear all
+close all
+clc 
+format long
+
+tic
+syms x;
+n = 5;
+% Initial Values.
+b = zeros(2*n,1);
+B = zeros(n);
+V = zeros(n);
+for i = 1:2*n
+    if (mod(i,2) ~= 0)
+        b(i) = 2/i;
+    end
+end
+for i = 1:n
+   for j = 1:n
+      B(i,j) = b(n+i-j); 
+   end
+end
+% Solve related system of equations.
+A = -B\b(n+1:2*n); 
+% Create a polynomial containing weight points.
+P = poly2sym([1;A],x)
+% Find weigt points.
+X = solve(P,x)
+for i = 1:n
+   for j = 1:n
+      V(i,j) = X(j)^(i-1);
+   end
+end
+% Find weights.
+C = V\b(1:n)
+toc

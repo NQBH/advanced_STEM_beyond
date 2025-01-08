4bdbf8066b25b1abd6c73801b8543f09cf2b71c6
NQBH <nguyenquanbahong@gmail.com>
2025-01-08 07:46:57 +0700

    plotted ReLU activation function

---
+++ machine_learning/Python/relu_plot.py
@@ -0,0 +1,9 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import plot_util
+
+ax = plot_util.setup_axis((-2,2), (-.5, 2))
+x = np.linspace(-2, 2, 100)
+ax.plot(x, tf.keras.activations.relu(x))
+plt.savefig("./figure/relu.pdf", bbox_inches = 'tight')

277c78452dc9490837670bef3c86cbd9cad998b7
NQBH <nguyenquanbahong@gmail.com>
2025-01-30 23:24:14 +0700

    added training perceptron

--- machine_learning/Python/ML_classifier_sklearn.py
+++ machine_learning/Python/ML_classifier_sklearn.py
@@ -12,3 +12,34 @@ X = iris.data[:, [2, 3]]
 y = iris.target
 
 print('Class labels:', np.unique(y))
+
+# Splitting data into 70% training and 30% test data:
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+
+print('Labels counts in y:', np.bincount(y))
+print('Labels counts in y_train:', np.bincount(y_train))
+print('Labels counts in y_test:', np.bincount(y_test))
+
+# Standardizing the features:
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# ## Training a perceptron via scikit-learn
+
+from sklearn.linear_model import Perceptron
+ppn = Perceptron(eta0=0.1, random_state=1)
+ppn.fit(X_train_std, y_train)
+
+y_pred = ppn.predict(X_test_std)
+print('Misclassified examples: %d' % (y_test != y_pred).sum())
+
+from sklearn.metrics import accuracy_score
+print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
+
+print('Accuracy: %.3f' % ppn.score(X_test_std, y_test))

b13602953aec35ae16ec917624f5173225d4154f
NQBH <nguyenquanbahong@gmail.com>
2025-01-31 01:26:26 +0700

    added decision region plot for logistic regression model

--- machine_learning/Python/ML_classifier_sklearn.py
+++ machine_learning/Python/ML_classifier_sklearn.py
@@ -43,3 +43,210 @@ from sklearn.metrics import accuracy_score
 print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
 
 print('Accuracy: %.3f' % ppn.score(X_test_std, y_test))
+
+# To check recent matplotlib compatibility
+
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=f'Class {cl}', 
+                    edgecolor='black')
+
+    # highlight test examples
+    if test_idx:
+        # plot all examples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='none',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100, 
+                    label='Test set')        
+
+# Training a perceptron model using the standardized training data:
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+plot_decision_regions(X=X_combined_std, y=y_combined,
+                      classifier=ppn, test_idx=range(105, 150))
+plt.xlabel('Petal length [standardized]')
+plt.ylabel('Petal width [standardized]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+#plt.savefig('figures/03_01.png', dpi=300)
+plt.show()
+
+# # Modeling class probabilities via logistic regression
+# ...
+# ### Logistic regression intuition and conditional probabilities
+
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+z = np.arange(-7, 7, 0.1)
+sigma_z = sigmoid(z)
+
+plt.plot(z, sigma_z)
+plt.axvline(0.0, color='k')
+plt.ylim(-0.1, 1.1)
+plt.xlabel('z')
+plt.ylabel('$\sigma (z)$')
+
+# y axis ticks and gridline
+plt.yticks([0.0, 0.5, 1.0])
+ax = plt.gca()
+ax.yaxis.grid(True)
+
+plt.tight_layout()
+#plt.savefig('figures/03_02.png', dpi=300)
+plt.show()
+
+# ### Learning the weights of the logistic loss function
+
+def loss_1(z):
+    return - np.log(sigmoid(z))
+
+
+def loss_0(z):
+    return - np.log(1 - sigmoid(z))
+
+z = np.arange(-10, 10, 0.1)
+sigma_z = sigmoid(z)
+
+c1 = [loss_1(x) for x in z]
+plt.plot(sigma_z, c1, label='L(w, b) if y=1')
+
+c0 = [loss_0(x) for x in z]
+plt.plot(sigma_z, c0, linestyle='--', label='L(w, b) if y=0')
+
+plt.ylim(0.0, 5.1)
+plt.xlim([0, 1])
+plt.xlabel('$\sigma(z)$')
+plt.ylabel('L(w, b)')
+plt.legend(loc='best')
+plt.tight_layout()
+#plt.savefig('figures/03_04.png', dpi=300)
+plt.show()
+
+class LogisticRegressionGD:
+    """Gradient descent-based logistic regression classifier.
+
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after training.
+    b_ : Scalar
+      Bias unit after fitting.
+    losses_ : list
+      Log loss function values in each epoch.
+
+    """
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+          Training vectors, where n_examples is the number of examples and
+          n_features is the number of features.
+        y : array-like, shape = [n_examples]
+          Target values.
+
+        Returns
+        -------
+        self : Instance of LogisticRegressionGD
+
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
+        self.b_ = np.float_(0.)
+        self.losses_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_ += self.eta * X.T.dot(errors) / X.shape[0]
+            self.b_ += self.eta * errors.mean()
+            loss = -y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output))) / X.shape[0]
+            self.losses_.append(loss)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, z):
+        """Compute logistic sigmoid activation"""
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
+X_train_01_subset = X_train_std[(y_train == 0) | (y_train == 1)]
+y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
+
+lrgd = LogisticRegressionGD(eta=0.3, n_iter=1000, random_state=1)
+lrgd.fit(X_train_01_subset,
+         y_train_01_subset)
+
+plot_decision_regions(X=X_train_01_subset, 
+                      y=y_train_01_subset,
+                      classifier=lrgd)
+
+plt.xlabel('Petal length [standardized]')
+plt.ylabel('Petal width [standardized]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+#plt.savefig('figures/03_05.png', dpi=300)
+plt.show()
+
+# ### Training a logistic regression model with scikit-learn

e09ae037dc4c7586434658ece63e2995a06c7ab7
NQBH <nguyenquanbahong@gmail.com>
2025-02-08 14:11:29 +0700

    added sympy det

--- VMC/Python/linear_algebra.py
+++ VMC/Python/linear_algebra.py
@@ -2,3 +2,10 @@ import numpy as np
 print(np.__version__)
 A = np.matrix([[1,0,1,0], [2,1,0,1], [1,0,1,0], [0,1,2,1]])
 print(np.linalg.matrix_rank(A))
+
+from sympy.matrices import Matrix, eye, zeros, ones, diag, GramSchmidt
+
+# VMC2023B1
+from sympy.abc import x, a
+A = Matrix([[x, a, a + 1], [a, a + 1, x], [a + 1, x, a]])
+print(A.det())

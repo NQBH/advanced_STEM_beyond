09e96ece36bf67e44b37af530a77b85bb3843365
NQBH <nguyenquanbahong@gmail.com>
2025-02-27 22:51:44 +0700

    C++: doubles: twice numbers in sequence

---
+++ OLP_ICPC/C++/double.cpp
@@ -0,0 +1,26 @@
+// source: [Wu_Wang2016, pp. 6--7]
+#include<iostream>
+using namespace std;
+int main() {
+	int i, j, n, count, a[20];
+	cin >> a[0]; // input 1st element
+	while (a[0] != -1) { // if it is not end of input, input a new test case
+		n = 1;
+		for ( ; ; ++n) {
+			cin >> a[n];
+			if (a[n] == 0) break;
+		}
+		count = 0; // determine how many items in each list are twice some other item
+		for (i = 0; i < n - 1; ++i) { // enumerate all pairs
+			for (j = i + 1; j < n; ++j) {
+				if (a[i]*2 == a[j] || a[j]*2 == a[i]) // accumulation
+					++count;
+			}
+		}
+		cout << count << endl; // output result
+		cin >> a[0]; // input 1st element of next test case
+	}
+	return 0;
+}
+// nqbh@nqbh-msi:~/advanced_STEM_beyond/OLP_ICPC/C++$ g++ -O2 -Wall double.cpp -o double
+// nqbh@nqbh-msi:~/advanced_STEM_beyond/OLP_ICPC/C++$ ./double < double.inp > double.out

17b1d68c30407266bf7257a617e17fb0a4e8c197
NQBH <nguyenquanbahong@gmail.com>
2025-02-27 20:28:18 +0700

    C++: financial management

--- OLP_ICPC/C++/financial_management.cpp
+++ OLP_ICPC/C++/financial_management.cpp
@@ -1,14 +1,14 @@
-// source: Wu_Wang2016
+// source: [Wu_Wang2016, p. 5]
 #include<iostream>
 using namespace std;
 int main() {
 	double avg, sum = 0.0, a[12] = {0};
-	for (int i = 0; i < 12; ++i) {
+	for (int i = 0; i < 12; ++i) { // input income of 12 motnhs a[0..11] & summation
 		cin >> a[i];
 		sum += a[i];
 	}
-	avg = sum/12;
-	printf("$%.2f", avg);
+	avg = sum/12; // compute average monthly
+	printf("$%.2f", avg); // output average monthly
 	return 0;
 }
 // nqbh@nqbh-msi:~/advanced_STEM_beyond/OLP_ICPC/C++$ g++ -O2 -Wall financial_management.cpp -o financial_management

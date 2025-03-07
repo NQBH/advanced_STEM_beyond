54d2b3790b4b5dbe952df2707864609caa7ccc5b
NQBH <nguyenquanbahong@gmail.com>
2025-03-08 02:54:50 +0700

    added in/output

--- CP_template.cpp
+++ CP_template.cpp
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+	// input & output
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
+	// handle input/output files
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	while (cin >> x) {
+		// code
+	}
+
+	return 0;
 }

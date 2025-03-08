129eab6f9d96c7037d7f9d0fb72a9354f60a645f
NQBH <nguyenquanbahong@gmail.com>
2025-03-08 20:16:19 +0700

    C++: doubles using map & vector

---
+++ OLP_ICPC/C++/double_DPAK.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+using namespace std;
+
+int main() {		
+	map<int, int> ma;
+	vector<int>a;
+
+	int n;
+	int ans = 0;
+	while (cin >> n) {
+		if (n == -1) {
+			break;
+		}
+		if (n == 0) {
+			for (int i : a) {
+				if (i % 2 == 1) continue;
+				ans += ma[i / 2];
+			}
+			cout << ans << endl;
+			ans = 0;
+			ma.clear();
+			a.clear();
+			continue;
+		}
+		a.push_back(n);
+		ma[n]++;
+	}
+}

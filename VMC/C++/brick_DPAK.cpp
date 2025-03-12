4419e64c9ed535dfa07c33e53e37e0279e6fb343
NQBH <nguyenquanbahong@gmail.com>
2025-03-12 08:46:49 +0700

    added counting bricks

---
+++ VMC/C++/brick_DPAK.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int numRow, numCol;
+vector<vector<int>>dp;
+int main() {	
+	cin >> numRow >> numCol;
+
+	// dp[i][j] : Cách chọn từ hàng 1 đến hàng i khi chọn viên gạch thứ j
+	
+	dp.resize(numRow + 1, vector<int>(numCol + 1, 0));
+
+	for (int i = 1; i <= numCol; i++) {
+		dp[1][i] = 1;
+	}
+
+	int ways = 0;
+	for (int row = 2; row <= numRow; row++) {
+		for (int col = 1; col <= numCol; col++) {
+			if (row % 2 == 0) {
+				if (col == 1) {
+					for (int BrickInPreviousRow = 2; BrickInPreviousRow <= numCol; BrickInPreviousRow++)
+						dp[row][col] += dp[row - 1][BrickInPreviousRow];
+				}
+				else {
+					for (int BrickInPreviousRow = 1; BrickInPreviousRow <= numCol; BrickInPreviousRow++) {
+						if (BrickInPreviousRow == col || BrickInPreviousRow == col - 1) {
+							
+						}
+						else {
+							dp[row][col] += dp[row - 1][BrickInPreviousRow];
+						}
+					}
+				}
+			}
+			else {
+				if (col == numCol) {
+					for (int BrickInPreviousRow = 1; BrickInPreviousRow < numCol; BrickInPreviousRow++)
+						dp[row][col] += dp[row - 1][BrickInPreviousRow];
+				}
+				else {
+					for (int BrickInPreviousRow = 1; BrickInPreviousRow <= numCol; BrickInPreviousRow++) {
+						if (BrickInPreviousRow == col || BrickInPreviousRow == col + 1) {
+
+						}
+						else {
+							dp[row][col] += dp[row - 1][BrickInPreviousRow];
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for (int row = 1; row <= numRow; row++) {
+		for (int col = 1; col <= numCol; col++) {
+			cout << dp[row][col] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 1; i <= numCol; i++) {
+		ways += dp[numRow][i];
+	}
+	cout << ways;
+}

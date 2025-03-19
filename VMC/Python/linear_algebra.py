76c46532a486295b0dfa2cafd866f69ea6e2873f
NQBH <nguyenquanbahong@gmail.com>
2025-03-19 13:53:26 +0700

    updated diagonalization

--- VMC/Python/linear_algebra.py
+++ VMC/Python/linear_algebra.py
@@ -20,3 +20,23 @@ A = Matrix([[1,a + 1,a + 2,0],[a + 3,1,0,a + 2],[a + 2,0,1,a + 1],[0,a + 2,a + 3
 detA = A.det()
 print(detA)
 print(factor(detA))
+
+# ky yeu VMC2024 Prob. 5.1
+# source: https://www.geeksforgeeks.org/python-sympy-matrix-diagonalize-method/
+from sympy import *
+X = Matrix([[1, 0, 0], [5, 2, 0], [4, 3, 2]])
+print("Matrix : {} ".format(X))
+print(X.eigenvals())
+# diagonalize X
+# P, D = X.diagonalize()
+# print("Diagonal of a matrix : {}".format(P))
+# print("Diagonal of a matrix : {}".format(D))
+
+# ky yeu VMC2024 Prob. 5.3
+A = Matrix([[7, -12, -2], [3, -4, 0], [-2, 0, -2]])
+print("Matrix : {} ".format(A))
+print(A.eigenvals())
+# diagonalize A
+P, D = A.diagonalize()
+print("Diagonal of a matrix : {}".format(P))
+print("Diagonal of a matrix : {}".format(D))

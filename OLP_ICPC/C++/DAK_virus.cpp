c8b92dba029afb919d379dd12a5c4853a7d63e0b
NQBH <nguyenquanbahong@gmail.com>
2025-05-21 14:19:54 +0700

    DAK C++: virus

--- OLP_ICPC/C++/DAK_virus.cpp
+++ OLP_ICPC/C++/DAK_virus.cpp
@@ -1,26 +1,21 @@
 #include <bits/stdc++.h>
-#include <complex>
 using namespace std;
 
-const int mod =1e9+7;
-int t;
-int n,k;
-/*
-flashback
-*/
+const int mod = 1e9+7;
+int t, n, k;
+
 void solve()
 {
     cin >> n >> k;
     int f[100100];
-    f[0]=n;
-    f[1]=2*n;
-    f[2]=5*n;
-    for (int i=3;i<=k;i++)
-    {
-            f[i]=f[i-1]*3%mod-f[i-2];
-    }
-    cout << f[k] << " ";
+    f[0] = n;
+    f[1] = 2 * n;
+    f[2] = 5 * n;
+    for (int i = 3; i <= k; i++)
+        f[i] = f[i - 1] * 3 % mod - f[i - 2];
+    cout << f[k] << "\n";
 }
+
 int main() {
     cin >> t;
     while (t--)

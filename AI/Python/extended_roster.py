6037c6ae2209f17cac166476178c45bddd389bbd
NQBH <nguyenquanbahong@gmail.com>
2025-05-31 13:25:12 +0700

    updated

--- AI/Python/extended_roster.py
+++ AI/Python/extended_roster.py
@@ -3,4 +3,18 @@ m, n = map(int, input().split())
 for i in range(m):
 	row = [int(x) for x in input().split()]
 	t.append(row)
-print(t)
+
+current_sum_working_time = [0] * m
+roster = [0] * n # machine with roster[i] take ith work
+
+for j in range(n):
+	current_sum_working_time_min = current_sum_working_time[0] + t[0][j]
+	for i in range(1, m):
+		tmp = current_sum_working_time[i] + t[i][j]
+		if tmp < current_sum_working_time_min:
+			current_sum_working_time_min = tmp
+			roster[j] = i
+		print("roster[j] = ", roster[j])
+	current_sum_working_time[roster[j]] += t[roster[j]][j]
+	print(current_sum_working_time)
+print(roster)
--- AI/Python/gradient.py
+++ AI/Python/gradient.py
@@ -30,7 +30,7 @@ if eta <= 0:
 else:
 	print(gradient_descent(x0, eta))
 
-# f1(x) = x^3 + 3x^2  + 5sin x - 7cos x + \sqrt{2}e^{-2x}
+# f1(x) = x^3 + 3x^2  + 5sin x - 7cos x + sqrt{2}e^{-2x}
 def f1(x):
 	return x**3 + 3*x**2 + 5*np.sin(x) - 7*np.cos(x) + np.sqrt(2)*np.exp(-2*x)
 def df1(x):
@@ -59,22 +59,26 @@ else:
 	print(gradient_descent_f1(x0, eta))
 
 # f(x,y) = 2x^3y^2 + sqrt(x^3)/y + sin(x^2y) + e^{cos(xy^2)}
-def f(x,y):
-	return 2*x**3*y**2 + sqrt(x**3) / y + np.sin(x**2*y) + np.exp(cos(x*y**2))
 
-def grad_f(x,y):
-	return None ### edit
+def f(x, y):
+    return 2*x**3*y**2 + np.sqrt(x**3)/y + np.sin(x**2 * y) + np.exp(np.cos(x * y**2))
+
+def grad_f(x, y):
+    df_dx = 6*x**2 * y**2 + (3/2) * x**0.5 / y + 2*x*y * np.cos(x**2 * y) - y**2 * np.sin(x * y**2) * np.exp(np.cos(x * y**2))
+    df_dy = 4*x**3 * y - np.sqrt(x**3) / y**2 + x**2 * np.cos(x**2 * y) - 2*x*y * np.sin(x * y**2) * np.exp(np.cos(x * y**2))
+    return np.array([df_dx, df_dy])
 
 x = float(input("x = "))
 y = float(input("y = "))
 print("f(x,y) = ", f(x,y))
-print("grad f(x,y) = ", gradf(x,y))
+print("grad f(x,y) = ", grad_f(x,y))
 
 def gradient_descent(x0, y0, eta):
 	### edit
+	return None
 
 x0 = float(input("x0 = "))
-yy0 = float(input("y0 = "))
+y0 = float(input("y0 = "))
 eta = float(input("eta = "))
 if eta <= 0:
 	print("error: eta must be positive!")

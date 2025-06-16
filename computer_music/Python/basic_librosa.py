f78ae129e2d7ce4142e2687d250bf9dbf312538f
NQBH <nguyenquanbahong@gmail.com>
2025-06-17 02:00:41 +0700

    added feature extraction

--- computer_music/Python/basic_librosa.py
+++ computer_music/Python/basic_librosa.py
@@ -16,3 +16,39 @@ print("Estimated tempo: " + str(list(map('{:.2f}%'.format, tempo))))
 
 # 4. Convert the frame indices of beat events into timestamps
 beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+
+# feature extraction example
+import numpy as np
+
+# Load the example clip
+y, sr = librosa.load(librosa.ex('nutcracker'))
+
+# Set the hop length; at 22050 Hz, 512 samples ~= 23ms
+hop_length = 512
+
+# Separate harmonics & percussives into 2 waveforms
+y_harmonic, y_percussive = librosa.effects.hpss(y)
+
+# Beat track on the percussive signal -- Nhịp điệu trên tín hiệu gõ
+tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr)
+
+# Compute MFCC features from the raw signal
+mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
+
+# & the first-order differences (delta features)
+mfcc_delta = librosa.feature.delta(mfcc)
+
+# Stack & synchronize between beat events
+# This time, we'll use the mean value (default) instead of median
+beat_mfcc_delta = librosa.util.sync(np.vstack([mfcc, mfcc_delta]), beat_frames)
+
+# Compute chroma features from the harmonic signal
+
+chromagram = librosa.feature.chroma_cqt(y=y_harmonic, sr=sr)
+
+# Aggregate chroma features between beat events
+# We'll use the median value of each feature between beat frames
+beat_chroma = librosa.util.sync(chromagram, beat_frames, aggregate=np.median)
+
+# Finally, stack all beat-synchronous features together
+beat_features = np.vstack([beat_chroma, beat_mfcc_delta])

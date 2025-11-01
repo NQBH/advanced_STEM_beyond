f5f6ebea3c04d8c5917b5fae3180732b3b8de8d7
NQBH <nguyenquanbahong@gmail.com>
2025-11-02 00:04:15 +0700

    updated CF800

---
+++ OLP_ICPC/Python/Proof_by_QED_trippi_troppi.py
@@ -0,0 +1,6 @@
+t = int(input())
+for _ in range(t):
+    inp = input()
+    for w in inp.split():
+        print(w[0],end="")
+    print()
---
+++ OLP_ICPC/Python/k1sara_Kamilka_sheep.py
@@ -0,0 +1,5 @@
+t = int(input())
+for test in range(t):
+    n = int(input())
+    a = [int(i) for i in input().split()]
+    print(max(a) - min(a))

989bd565f3d2c61ec1e1768432ff842ca3c882c0
NQBH <nguyenquanbahong@gmail.com>
2026-05-11 12:14:23 +0700

    added bounded sequence

---
+++ analysis/C++/NQBH_bounded_sequence.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	double M = 0; // initialization of bound M: M < 0: nonlogical since M >= |a_n| >= 0
+	vector<double> a(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+		M = max(M, abs(a[i]));
+	}
+	cout << "The sequence {a_i}_{i=0}^{n-1} is bounded by " << M + 1 << " since |a_i| <= " << M + 1 << " for all i = 0,1,...," << n - 1 << ".\n";
+}
